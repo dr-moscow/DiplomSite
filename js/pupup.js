e3398d9168d5fc7f0395d6e3db15ef61bc09203d
dr-moscow <dr.moscow@protonmail.com>
2023-02-11 12:18:16 +0300

    Поправили расположение скриптов

--- js/pupup.js
+++ js/pupup.js
@@ -1,6 +1,6 @@
-let buttLogin = document.querySelector(".login")
-let buttLogPopup = document.querySelector(".login-popup")
-buttLogin.addEventListener("click", showPupUp)
+let buttLogin = document.querySelector(".login");
+let buttLogPopup = document.querySelector(".login-popup");
+buttLogin.addEventListener("click", showPupUp);
 function showPupUp(event) {
 	event.preventDefault();
 	buttLogPopup.classList.toggle("show-popup")
